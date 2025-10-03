58419c85afb93eb55f34b7487f70eb3c5efe6dbb
CAMILI VITÓRIA FIAMONCINI <camili_fiamoncini@estudante.sesisenai.org.br>
2025-10-03 09:48:25 -0300

    aula 1: comando print

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Isso é um comentário
+# Vamos escapar da maldição
+# Esse é o comando print,
+#ele serve para imprimir na tela
+
+print ("Olá mundo!")
+
+# Podemos mudar o comportamento da finalização
+# do comando com o parametro end
+print("Esse é o curso de: ", end= "\n ")
+print("Lógica", "de",  "programação")
+# Podemos mudar o comportamento do separador
+#com o parametro sep
+print("Frase" , "com" , " hifen" , sep= "-")
